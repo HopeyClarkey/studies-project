4f57e623189bc515dadc60710d7a8f1ed6109a73
Hope Clarke <thehopeclarke@gmail.com>
2025-08-29 17:40:20 -0500

    added skeleton

--- datatypes.js
+++ datatypes.js
@@ -3,4 +3,29 @@
  * 
  * 0: (introduction)
  * 
+ * 1) Number
+ * 
+ * 2) String
+ * 
+ * 3) Boolean
+ * 
+ * 4) Array
+ * 
+ * 5) Object
+ * 
+ * 6) Function
+ * 
+ * 7) Undefined
+ * 
+ * 8) Null
+ * 
+ * 9) NaN
+ * 
+ * 10) Infinity and -Infinity
+ * 
+ * 11) Difference betweeen primitive/simple and complex datatypes
+ * 
+ * 12) Primitive values are passed to a function BY COPY, and complex value types BY REFERENCE. What does that mean, and how are they different
+ * 
+ * 
  */
--- variables.js
+++ variables.js
@@ -3,4 +3,8 @@
  * 
  * 0: (provide general description of the concept)
  * 
+ * 1) Delcaration and assignment
+ * 2) var, let, & const
+ * 3) Hoisting
+ * 
  */
