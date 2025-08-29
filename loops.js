e59f14db0529c84974e174b6f9faa949a04f3885
Hope Clarke <thehopeclarke@gmail.com>
2025-08-29 18:40:39 -0500

    added 1 & 2, example 2a

--- loops.js
+++ loops.js
@@ -3,17 +3,94 @@
  * 
  * 0: (introduction)
  * 
- * 1) Explain while, for and for - in loops
+ * 1) Explain while, for and for - in loops:
  * 
- *    a while loop is a loop that executes with the keyword 'while', a parenthesis, and a condition that while true, whill continue to execute the block of code in the body of the while-loop.
- *    for loop is a loop that will move forward from an initialized index, to another index (found with a condition), and an increment statement
- *    for-in loop will pull all of the keys out of an Object, one by one. This allows you to use the key to get the values in the Object. You must use array syntax when accessing keys in an object.
+ *                          while loop                                                                                                                           
+ *    a while loop is a loop that executes with the keyword 'while',                                           
+ *    a parenthesis, and a condition that while true, will continue                                                  
+ *    to execute the block of code in the body of the while-loop.                                                                                        
+ *    
+ *  
+ *                          for loop                            
+ *    for loop is a loop that will move forward from an initialized   
+ *    index, to another index (found with a condition), and an
+ *    increment statement
+ *    
+ *  
+ *                          for- in loop                      
+ *    for-in loop will pull all of the keys out of an Object,
+ *    one by one. This allows you to use the key to get the values
+ *    to execute the block of code in the body of the while-loop.                                                                                        
+ *    
  * 
- * 2) Be able to loop any number of  times, forward counting up to some limit, backward counting down to 0
+ * 2) Be able to loop any number of  times, forward counting up to some limit,
+ *    and then backward counting down to 0
+ * 
+ *      Example A: 
+ *          declare an array variable to loop through
+ *          use a for loop to loop through the array XX times
+ *          print to the console each loop
+ * 
+ *      Example B:
+ *          
+ * 
  * 
  * 3) Loop over and Array, forwards and backwards
  * 
+ *      Example A:
+ * 
+ * 
+ *      Example B:
+ * 
+ * 
+ * 
  * 4) Loop over an Object
  * 
+ *      Example A:
  *
+ * 
+ * 
+ * 
  */
+    var log = console.log;
+
+//      Example 2)A:
+
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+    //for loop to loop through the array in order to print the numbers 1- 13 to the console
+        for (var i = 0; i <= 13; i++){
+            log(i);
+        }
+
+
+
+
+
+//      Example 2)B:
+
+
+
+
+
+
+
+//      Example 3)A:
+
+
+
+
+
+
+
+//      Example 3)B:
+
+
+
+
+
+
+//      Example 4)A
+
+
+
