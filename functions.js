fd74ac57fa028bacda7e668f90d9ac1cbe2f3ae2
Hope Clarke <thehopeclarke@gmail.com>
2025-08-29 17:54:43 -0500

    added skeleton

--- functions.js
+++ functions.js
@@ -3,4 +3,19 @@
  * 
  * 0: (introduction)
  * 
+ *  1)  The two phases to using functions: First we must ____? Next we can execture (or two other words for executing a function?) a function by?
+ * 
+ *  2) What's the difference between a functions parameters and arguements PASSED to a function?
+ * 
+ *  3) What's the syntax for a Named function?
+ * 
+ *  4) How do we assign a function to a variable?
+ * 
+ *  5) Functions and OPTIONALLY take inputs, and OPTIONALLY return a single value. How do we specificy inputs, and how do we specify outputs?
+ * 
+ *  6) Scope: Functions can see and modify variables in parent or global scopes. The inverse is NOT true.
+ * 
+ *  7) closures: Functions form closures areound the data they house. If an object returned from the Function and is held in memory somewhere (referenced), that closure stays ALIVE, and data can continue to exist in these closures.
+ * 
+ * 
  */
