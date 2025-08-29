9c1bc68d0588faf3b7d58a63d162a2de16eedc59
Hope Clarke <thehopeclarke@gmail.com>
2025-08-29 17:48:59 -0500

    added skeleton

--- loops.js
+++ loops.js
@@ -3,4 +3,13 @@
  * 
  * 0: (introduction)
  * 
+ * 1) Explain while, for and for - in loops
+ * 
+ * 2) Be able to loop any number of  times, forward counting up to some limit, backward counting down to 0
+ * 
+ * 3) Loop over and Array, forwards and backwards
+ * 
+ * 4) Loop over an Object
+ * 
+ *
  */
