5f85a2e5f0ba81fc2f639860d3f38892922c06b5
Hope Clarke <thehopeclarke@gmail.com>
2025-08-29 17:43:27 -0500

    added skeleton

--- operators.js
+++ operators.js
@@ -3,4 +3,19 @@
  * 
  * 0: (introduction)
  * 
+ * 
+ * 
+ * 1) Assignment Operators
+ * 
+ * 2) Arithmetic Operators
+ * 
+ * 3) Comparison Operators
+ * 
+ * 4) Logical Operators
+ * 
+ * 5) Unary Operators (!, typeOf, -)
+ * 
+ * 6) Ternary Operators (a ? b : )
+ * 
+ * 
  */
