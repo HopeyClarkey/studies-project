b93bee15e8b3c3b8f8673d4142ff800434e20f66
Hope Clarke <thehopeclarke@gmail.com>
2025-08-31 14:58:14 -0500

    added more info

--- loops.js
+++ loops.js
@@ -23,17 +23,17 @@
  *    to execute the block of code in the body of the while-loop.                                                                                        
  *    
  * 
- * 2) Be able to loop any number of  times, forward counting up to some limit,
- *    and then backward counting down to 0
+ * 2) Be able to loop any number of  times, forward counting up to 
+ *    some limit,and then backward, counting down to 0
  * 
  *      Example A: 
- *          declare an array variable to loop through
- *          use a for loop to loop through the array XX times until condition is met
- *          print to the console each loop
+ *          ~  declare an array variable to loop through
+ *          ~  use a for loop to loop through the array XX times until condition is met
+ *          ~  print to the console each loop
  * 
  *      Example B:
- *          use a for loop to loop backward, starting at the .length of the array
- *          print to the console each loop
+ *          ~  use a for loop to loop backward, starting at the .length of the array
+ *          ~  print to the console each loop
  * 
  * 
  * 3) Loop over an Array, forwards and backwards
@@ -58,27 +58,24 @@
 
 //      Example 2)A:
 
-        for (var i = 0; i <= 13; i++){           // for loop to loop through the array in 
-            log(i);                             // order to print the numbers 1- 13
-        };
+        for (var i = 0; i <= 13; i++){          // for loop to loop through the array, starting 
+            log(i);                             // at index 0 through 13 by 1, in order to print 
+        };                                      // the numbers 1- 13 to the console.
 
 //      Example 2)B:
 
-        for (var i = arr.length; i >= 0; i--){   // for loop to loop back through the array in 
-            log(i);                             // order to print the numbers 15-0
-        };
-
+        for (var i = 7; i >= 0; i--){          // for loop to loop back through the array, in 
+            log(i);                             // starting at index 7 back down to 0 by 1, in 
+        };                                      // order to print the numbers 7-0 to the console.
 
 //      Example 3)A:
 
-
-
-
-
-
+        for (var i = 0; i < arr.length; i++){   // for loop to loop through array, starting at 
+            log(i);                             // index 0, up to the length of the array, by 1,
+        }                                       // in order to print the numbers 1-15 to the console.
 
 //      Example 3)B:
-
+                
 
 
 
