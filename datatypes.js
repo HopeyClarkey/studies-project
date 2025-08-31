a0a047ce42c1339b8ee70d672a4104f9c5988fc4
Hope Clarke <thehopeclarke@gmail.com>
2025-08-31 12:53:57 -0500

    added description

--- datatypes.js
+++ datatypes.js
@@ -3,23 +3,23 @@
  * 
  * 0: (introduction)
  * 
- * 1) Number
+ * 1) Number - simple/primitive datatype, represents a numeric value or integer
  * 
- * 2) String
+ * 2) String - simple/primitive datatype, represents a string of characters, commonly used for text
  * 
- * 3) Boolean
+ * 3) Boolean - simple/primitive datatype, represents a true/false value
  * 
- * 4) Array
+ * 4) Array - complex datatype, represents a list of data, contained within a []
  * 
- * 5) Object
+ * 5) Object - complex datatype, represents a group of data subgrouped by keys
  * 
- * 6) Function
+ * 6) Function - complex datatype
  * 
- * 7) Undefined
+ * 7) Undefined - simple/primitive datatype
  * 
- * 8) Null
+ * 8) Null - simple/primitive datatype
  * 
- * 9) NaN
+ * 9) NaN - simple/primitive datatype
  * 
  * 10) Infinity and -Infinity
  * 
