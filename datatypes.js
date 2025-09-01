dc594119d96d9c4a346de17927406ad3a166bef9
Hope Clarke <thehopeclarke@gmail.com>
2025-09-01 10:45:56 -0500

    c

--- datatypes.js
+++ datatypes.js
@@ -1,31 +1,63 @@
 /**
  * DATATYPES:
  * 
- * 0: (introduction)
+ * 0: Javascript has several major categories to describe a value's type:
  * 
- * 1) Number - simple/primitive datatype, represents a numeric value or integer
  * 
- * 2) String - simple/primitive datatype, represents a string of characters, commonly used for text
  * 
- * 3) Boolean - simple/primitive datatype, represents a true/false value
+ * 1)                               Number: 
+ *    simple/primitive datatype, represents a numeric value or integer.
  * 
- * 4) Array - complex datatype, represents a list of data, contained within a []
+ * 2)                               String:
+ *       simple/primitive datatype, represents a string of characters,
+ *                          commonly used for text.
  * 
- * 5) Object - complex datatype, represents a group of data subgrouped by keys
+ * 3)                               Boolean: 
+ *          simple/primitive datatype, represents a true/false value.
  * 
- * 6) Function - complex datatype
+ * 4)                                Array: 
+ *      complex datatype, represents a list of data, contained within a [].
  * 
- * 7) Undefined - simple/primitive datatype
+ * 5)                                Object: 
+ *                 complex datatype, represents a group of data 
+ *                        subgrouped by keys & values
  * 
- * 8) Null - simple/primitive datatype
+ * 6)                               Function:
+ *                 complex datatype, represents any number of 
+ *                      statements that work with data.
+ * 
+ * 7)                              Undefined:
+ *               simple/primitive datatype- it exists, but has no value
+ * 
+ * 8)                                 Null:
+ *                          simple/primitive datatype, no data at all.
  * 
  * 9) NaN - simple/primitive datatype
  * 
  * 10) Infinity and -Infinity
  * 
  * 11) Difference betweeen primitive/simple and complex datatypes
+ *                         Simple datatypes: 
+ *    Are immutable, they do not hold, collect, or aggregate other values, 
+ *    and operations on simple datatypes return simple values, they do not 
+ *    alter the original value. 
+
  * 
- * 12) Primitive values are passed to a function BY COPY, and complex value types BY REFERENCE. What does that mean, and how are they different
+ *                             Complex datatypes:
+ *    Are mutable, the aggregate other values and therefore are of indefinate 
+ *    size. Objects can contain any number of key:value pairs. Arrays can 
+ *    have any number of elements. Functions can have any number of statements.
+ * 
+ * 12) Primitive values are passed to a function BY COPY, 
+ * and complex value types BY REFERENCE. 
+ * 
+ *    Simple value types are 'copy-by-value'- when assigning or passing, 
+ *    simple data-types are copied from one variable to the next. Variables 
+ *    can only directly contain simple types, because the maximum amount of 
+ *    memory a simple datatype is is 8 bites, which is the amount
+ *    each variable takes up in memory.
+ *    Complex datatypes are 'copy-by-reference'- when assigning or passing, 
+ *    complex data-types are passed by reference to the data.
  * 
  * 
  */
