b9e72fea08db59c3998c6b8da2c80258701f7bb4
Hope Clarke <thehopeclarke@gmail.com>
2025-09-01 10:19:18 -0500

    update description

--- variables.js
+++ variables.js
@@ -2,15 +2,37 @@
  * VARIABLES:
  * 
  * 0: In order to create and keep things in memory, you have to give them 
- * a name: this is called declaring a variable. A variable is an identifier
- * that 'points' to a value of some datatype. Datatypes can be Numbers, Strings
- * Arrays, Objects, or more. Variable means that we can change the value, and
- * type of value to which they point.
+ *    a name. These containers that 'point' to a value of some datatype are 
+ *    called variables. Datatypes can be Numbers, Strings Arrays, Objects, 
+ *    etc. Variable means that we can change the value, and even the type 
+ *    of value to which they point.
  * 
- * 1) Delcaration and Assignment:
  * 
- * 2) var, let, & const
+ * 1)                       Delcaration and Assignment: 
+ *    Declaring a variable is done with var, which means it now exists. 
+ *    Assigning-or initializing a variable is one more step. It means 
+ *    giving the variable a value. 
  * 
- * 3) Hoisting
+ * 
+ * 2)                           var, let, & const:
+ *    var was original way to declare a variable, but let and const added in
+ *    2015. Differenciated by re-assignment type & hoisting scope. var has no
+ *    restrictions on reassignment and var will hoist initialization value to 
+ *    scope. let also allows you to reassign variable throughout, and const 
+ *    will not allow you to to reassign.  let and const will hoist to scope, 
+ *    but will give undefined as value(some consider this non-hoisting???).
+ * 
+ * 
+ * 3)                               Hoisting:
+ *    Hoisting refers to being able to ust a variables value in it's scope 
+ *    before the line it was declared. The interpreter appears to move the 
+ *    declaration of functions, variables, classes(?) or imports(??) to the
+ *    top of their scope, prior to the execution of the code.
  * 
  */
+
+// 1) Declare a variable 
+var petName; //variable created but not assigned
+petName = 'Logan'; //assigned string "Logan" to petName
+
+//2)
