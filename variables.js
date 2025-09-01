df161240d4bf3ac6deb6502b72f7c204847250fe
Hope Clarke <thehopeclarke@gmail.com>
2025-08-31 19:14:02 -0500

    update 0

--- variables.js
+++ variables.js
@@ -1,8 +1,11 @@
 /**
  * VARIABLES:
  * 
- * 0: (provide general description of the concept)
- * 
+ * 0: In order to create and keep things in memory, you have to give them 
+ * a name: this is called declaring a variable. A variable is an identifier
+ * that 'points' to a value of some datatype. Datatypes can be Numbers, Strings
+ * Arrays, Objects, or more. Variable means that we can change the value, and
+ * type of value to which they point.
  * 
  * 1) Delcaration and Assignment:
  * 
