cda2585dd39267d4a5c62439f7b8c3191910e9ab
Hope Clarke <thehopeclarke@gmail.com>
2025-09-03 20:52:31 -0500

    added descript

--- control-flow.js
+++ control-flow.js
@@ -1,7 +1,15 @@
 /**
  * CONTROL FLOW:
  * 
- * 0: (introduction)
+ * 0: Conditional statements allow us to control the flow of our application.
+ * we can make decisions in our code by asking true or false questions. Based
+ * on the answer we decide what code runs if the question is true, and what
+ * code runs if the question is false. To do this, we use condtional statements
+ * These are if-statements and their chains, if-elseif-else. We can also use 
+ * switch statements similarly to control flow.
+ * 
+ * In javascript, condtional statements boil down to comparisons and or values 
+ * based on their truthy ness
  * 
  * 1) If
  * 
