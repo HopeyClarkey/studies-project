7b45c0a22fe38946334d532a7755100253d06d96
Hope Clarke <thehopeclarke@gmail.com>
2025-09-26 00:16:08 -0500

    finished control flow bones

--- control-flow.js
+++ control-flow.js
@@ -13,12 +13,29 @@
  * into boolean, this means the value is evaluated in a boolean context.
  * 
  * 1) If
+ *      The if statement specifies a block of code to be executed if a condition is true
  * 
  * 2) Else-If
+ *      The else if statement specifies a new condition if the first condition is false
  * 
  * 3) Else
+ *      The else statement specifies a block of code to be executed if the condition is false
  * 
  * 4) Switch
- * 
- * 
+ *      The switch statement executes a block of code depending on different cases. Use switch
+ *  to select one of many blocks of code to be executed. Switch statements evaluate an 
+ *  expression, the value of the expression is then compared with the values of
+ *  each case. If there is a match, the associated block of code is executed.
+ *      Used with a break; keyword
+ *          ~   The break; keyword breaks out of the switch block. 
+ *          ~   This will stop the execution of code or the case 
+ *              testing inside the block. 
+ *          ~   If a break is ommitted, the next code block in the switch
+ *              statement is executed.
+ *      Used with a default keyword
+ *          ~   The default keyword specifies some code to run if there
+ *          ~   is no case match.
+ *          ~   There can be only one default keyword in a switch
+ *          ~   Is optional, but recommended as it takes care of 
+ *              unexpected cases.
  */
