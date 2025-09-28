8bdde0aa09f98f16458dc9d30f5f801762d669ab
Hope Clarke <thehopeclarke@gmail.com>
2025-09-27 20:12:33 -0500

    function bones

--- functions.js
+++ functions.js
@@ -1,21 +1,37 @@
 /**
  * FUNCTIONS:
  * 
- * 0: (introduction)
+ * 0: Functions are a fundemental building block of Javascript. A function in javascript is 
+ * similar to a procedure- a set of statements that performs a task or calculates a value,
+ * but for a procedure to qualify as a function, it should take some input and return an
+ * output where there is some obvious relationship between the input and the output. To
+ * use a function, you must define it somewhere in the scope from which you wish to call it
  * 
- *  1)  The two phases to using functions: First we must ____? Next we can execture (or two other words for executing a function?) a function by?
+ *  1) The two phases to using functions: First we must define the function by giving it a name 
+ *     Next we can execute, invoke, or call a function by using that name.
  * 
- *  2) What's the difference between a functions parameters and arguements PASSED to a function?
+ *  2) Parameters refer to the variables listed in a function's declaration, defining the 
+ *  input that the function can accept. Arguments, however, are the actual values passed to 
+ *  the function when it is called, filling the parameters during execution.
  * 
- *  3) What's the syntax for a Named function?
+ *  3) A named function can be created like this :
+ *          function nameOfFunction (){
+ *              body of function
+ *          }
  * 
- *  4) How do we assign a function to a variable?
+ *  4) How to assign a function to a variable:
+ *          let nameOfFunctionToo = function(){
+ *          }
  * 
- *  5) Functions and OPTIONALLY take inputs, and OPTIONALLY return a single value. How do we specificy inputs, and how do we specify outputs?
+ *  5) Functions take inputs, and return a single value. Inputs are specified as parameters
+ *  when writing the function, and outputs are defined by the 'return' statement.
  * 
- *  6) Scope: Functions can see and modify variables in parent or global scopes. The inverse is NOT true.
+ *  6) Scope: Functions can see and modify variables in parent or global scopes. 
+ *  The inverse is NOT true.
  * 
- *  7) closures: Functions form closures areound the data they house. If an object returned from the Function and is held in memory somewhere (referenced), that closure stays ALIVE, and data can continue to exist in these closures.
+ *  7) closures: Functions form closures areound the data they house. If an object returned 
+ *  from the Function and is held in memory somewhere (referenced), that closure stays ALIVE,
+ *  and data can continue to exist in these closures.
  * 
  * 
  */
